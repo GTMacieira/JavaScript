703d661ab526ad7e28cc44e6b17c2d23102d3c2a
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-04 21:52:16 -0300

    condicionais e variaveis

---
+++ script.js
@@ -0,0 +1,37 @@
+//document.getElementById("titulo").innerHTML="Olá! Tuo bem ?";
+
+//document.write('Algum texto');
+
+//window.alert('Exemplo');
+
+//console.log('Exemplo no consolo')
+
+//var nome = 'Guilherme';
+//var idade = 90;
+
+//var x = 10;
+//var y = 15;
+
+//var total = x+y;
+
+//alert(total);
+
+//var nome = 'Guilherme'
+//var sobrenome = 'Macieira'
+
+//var nome_completo = nome +' '+sobrenome
+
+//alert(nome_completo)
+
+var hora = 15;
+
+if(hora < 12) {
+    console.log('Bom dia!')
+}else if( hora <18){
+    console.log('Boa tarde!')
+}else if(hora <=23 && hora > 18)
+
+if ( hora == 12 || hora == 18){
+    console.log ('Estamos na hora errada na rua')
+}
+

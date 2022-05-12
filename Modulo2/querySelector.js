542b231f94d88180fc622938da179baa4332145f
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-11 22:48:02 -0300

    querySelector

---
+++ Modulo2/querySelector.js
@@ -0,0 +1,2 @@
+document.querySelector('#exemplo') /*É possivel localizar os elementos não importando se é class, id, .... */
+document.querySelectorAll() /*Localizaq todos os elementos procurados */

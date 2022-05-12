3c18d04a3e64871b38e08576b97831cdf354d2e4
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-11 22:40:29 -0300

    Selecionar_elementos

---
+++ Modulo2/script.js
@@ -0,0 +1,4 @@
+document.getElementById('exemplo').innerHTML = "Macieira <button>bot</button>"; /* seleciona pelo ID */
+document.getElementsByClassName('lista')[0].innerHTML = 'Item aLterado'; /* seleciona pela class */
+document.getElementsByTagName('button')/*localiza pelo nome da TAG */
+document.getElementsByName('telefone')/*localiza pelo name*/

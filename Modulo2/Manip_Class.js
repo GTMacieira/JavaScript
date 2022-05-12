5c26cc2b75ad20b7cff5ecaaa35a0b881caa2ad2
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-11 23:28:45 -0300

    Manipulando_Class_2

--- Modulo2/Manip_Class.js
+++ Modulo2/Manip_Class.js
@@ -1,17 +1,29 @@
-function verde(){
-    document.querySelector('#exemplo').classList.remove('vermelho');
-    document.querySelector('#exemplo').classList.remove('azul');
-    document.querySelector('#exemplo').classList.add('verde');
+function trocar() {
+    if(document.querySelector('button').classList.contains('preto')){
+        document.querySelector('button').classList.remove('preto');
+        document.querySelector('button').classList.add('verde');
+    } else {
+        document.querySelector('button').classList.remove('verde');
+        document.querySelector('button').classList.add('preto');
+    }
 }
 
-function vermelho(){
-    document.querySelector('#exemplo').classList.remove('verde');
-    document.querySelector('#exemplo').classList.remove('azul');
-    document.querySelector('#exemplo').classList.add('vermelho');
-}
 
-function azul(){
-    document.querySelector('#exemplo').classList.remove('vermelho');
-    document.querySelector('#exemplo').classList.remove('verde');
-    document.querySelector('#exemplo').classList.add('azul');
-}
+
+// function verde(){
+//     document.querySelector('#exemplo').classList.remove('vermelho');
+//     document.querySelector('#exemplo').classList.remove('azul');
+//     document.querySelector('#exemplo').classList.add('verde');
+// }
+
+// function vermelho(){
+//     document.querySelector('#exemplo').classList.remove('verde');
+//     document.querySelector('#exemplo').classList.remove('azul');
+//     document.querySelector('#exemplo').classList.add('vermelho');
+// }
+
+// function azul(){
+//     document.querySelector('#exemplo').classList.remove('vermelho');
+//     document.querySelector('#exemplo').classList.remove('verde');
+//     document.querySelector('#exemplo').classList.add('azul');
+// }

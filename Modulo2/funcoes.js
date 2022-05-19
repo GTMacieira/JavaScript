93970a6e3e707ac925dbb55ad73a5355fd65bee3
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-18 22:25:37 -0300

    Funcoes2

--- Modulo2/funcoes.js
+++ Modulo2/funcoes.js
@@ -1,6 +1,13 @@
-function alterar (){
-    document.getElementById("titulo").innerHTML = "Trocou o titulo"
-    document.getElementById("campo").value = "Trocou o campo"
+function alterar (titulo){
+    document.getElementById("titulo").innerHTML = titulo
+    document.getElementById("campo").value = titulo
 }
 
-//alterar();
+function somar (x, y){
+    let total = x + y; //var global, let apenas dentro da variavel
+    return total
+}
+
+//alterar("Frase de exemplo")
+
+var resultado = somar(1, 99);
